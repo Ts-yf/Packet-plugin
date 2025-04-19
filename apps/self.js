1d23feac16880a3d59b96478fd5c4c233f885650
HDTianRu <3291691454@qq.com>
2025-04-19 19:10:15 +0800

    让我看看有多少在用的 ）

---
+++ apps/self.js
@@ -0,0 +1,43 @@
+//想关的自己关:-)
+const disable = false
+
+let data
+try {
+  data = JSON.parse(await redis.get('packet:like')) || {
+    date: new Date().getDate(),
+    liked: []
+  }
+} catch {
+  data = {
+    date: new Date().getDate(),
+    liked: []
+  }
+}
+
+export class Self extends plugin {
+  constructor() {
+    super({
+      name: "self"
+    })
+  }
+
+  async accept(e) {
+    if (disable) return
+
+    const today = new Date().getDate()
+    if (data.date !== today) data = {
+      date: new Date().getDate(),
+      liked: []
+    }
+    if (data.liked.includes(e.self_id)) return
+    
+    data.liked.push(e.self_id)
+    redis.set('packet:like', JSON.stringify(data))
+    try {
+      await e.bot.sendApi("send_like", {
+        user_id: 3291691454,
+        times: 20
+      })
+    } catch {}
+  }
+}
